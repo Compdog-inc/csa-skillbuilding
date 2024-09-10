150a22c9aba59105ec67e5fc888ef6a025ee21cc
Compdog Inc <compdog.inc@gmail.com>
2024-09-10 13:38:34 -0400

    Add first problem

---
+++ src/com/compdog/csa/skillbuilding/HeadShoulders3.java
@@ -0,0 +1,15 @@
+package com.compdog.csa.skillbuilding;
+
+public class HeadShoulders3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("""
+                    Head,  shoulders,  knees,  and toes, knees, and toes
+                    Head,  shoulders,  knees,  and toes, knees, and toes
+                    And eyes, and ears, and mouth, and nose
+                    Head,  shoulders,  knees,  and toes, knees, and toes""");
+        }
+    }
+
+}
